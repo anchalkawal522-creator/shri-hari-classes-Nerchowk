86c11ca8d2081a214e95207f22462e4851055892
Anchal Katwal <anchalkawal522@gmail.com>
2026-06-27 20:42:06 +0530

    Fix file names

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -3,7 +3,7 @@ import COURSES from "../../components/COURSES/Courses.jsx";
 import WHYUS from "../../components/WHYUS/Whyus.jsx";
 import TOPWHY from "../../components/TOPWHY/Whyustop.jsx";
 import INSTA from "../../components/INSTA/Instagram.jsx";
-export default function home() {
+export default function Home() {
   return (
     <>
       <HERO />
