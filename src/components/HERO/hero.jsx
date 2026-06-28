67fb43dba3f4980eaed4ed76183a23fa033947b1
Anchal Katwal <anchalkawal522@gmail.com>
2026-06-28 12:08:40 +0530

    add google verification meta tag

--- src/components/HERO/hero.jsx
+++ src/components/HERO/hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
             <div className="left">
                 <div className="content-hero">
 
-                    <h1>shri hari<br /><span className='name-i'>classes</span></h1>
+                    <h1>sri hari<br /><span className='name-i'>classes</span></h1>
                     <p className="goal">learn <span className='list-h'> • </span> achive<span> • </span> succeed</p>
                     <p className="peh-h">Quality education, experienced teachers and a supportive environment to help students achieve their dream</p>
                      <div className="btn-hero">
