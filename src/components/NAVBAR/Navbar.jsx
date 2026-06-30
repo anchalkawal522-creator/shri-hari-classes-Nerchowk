c8982824d21f19a939a4410c025051882d7b7dfd
Anchal Katwal <anchalkawal522@gmail.com>
2026-06-30 17:00:21 +0530

    fix footer

--- src/components/NAVBAR/Navbar.jsx
+++ src/components/NAVBAR/Navbar.jsx
@@ -1,11 +1,9 @@
 import "./nav.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 import logo from "../../assets/image.png";
 
 function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
 
   const openWhatsApp = () => {
     const phoneNumber = "916230020555";
@@ -14,14 +12,6 @@ function Navbar() {
 
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, "_blank");
-  };
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const closeMenu = () => {
-    setMenuOpen(false);
   };
 
   return (
@@ -33,23 +23,29 @@ function Navbar() {
             <img src={logo} alt="Logo" />
           </div>
 
-          {/* Toggle Button */}
-          <div className="toggle-btn" onClick={toggleMenu}>
-            ☰
-          </div>
-
-          <div className={`menu ${menuOpen ? "active" : ""}`}>
+          <div className="menu">
             <ul>
-              <li onClick={closeMenu}><Link to="/">Home</Link></li>
-              <li onClick={closeMenu}><Link to="/about">About</Link></li>
-              <li onClick={closeMenu}><Link to="/courses">Courses</Link></li>
-              <li onClick={closeMenu}><Link to="/facilities">Facilities</Link></li>
-              <li onClick={closeMenu}><Link to="/gallery">Gallery</Link></li>
-              <li onClick={closeMenu}><Link to="/contact">Contact</Link></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/courses">Courses</Link></li>
+              <li><Link to="/facilities">Facilities</Link></li>
+              <li><Link to="/gallery">Gallery</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
 
-              <button onClick={openWhatsApp}>
+              <button
+                onClick={openWhatsApp}
+                style={{
+                  background: "#25D366",
+                  color: "white",
+                  border: "none",
+                  padding: "10px 18px",
+                  borderRadius: "8px",
+                  cursor: "pointer",
+                }}
+              >
                 WhatsApp
               </button>
+
             </ul>
           </div>
 
